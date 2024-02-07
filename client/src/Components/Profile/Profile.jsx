45cd2237d9db47cf4c1965250c3e275f05cb2942
kumarpritam1468 <manoharipritam@gmail.com>
2024-02-07 19:52:42 +0530

    add profile page

--- client/src/Components/Profile/Profile.jsx
+++ client/src/Components/Profile/Profile.jsx
@@ -1,20 +1,44 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import boxImg from '../../assets/box-img.jpg';
+import { MdPermPhoneMsg } from "react-icons/md";
+import { MdArrowOutward } from "react-icons/md";
 import './Profile.css';
-// import '../Hero/Hero.css';
 
 const Profile = () => {
     return (
         <div className='main-bg'>
             <Navbar />
             <div className="profile">
-                <div className="profileCard">
-                    <div className='profileName'>
-                        <h1>Your <span className="colorText">Name</span></h1>
-                        <h2 className='colorText'>Doctor/Patient</h2>
+                <div>
+                    <h1>Your <span className="colorText">Name</span></h1>
+                    <h2 className='colorText'>Doctor/Patient</h2>
+                </div>
+
+                <div className="serviceBoxes profileBoxes">
+                    <div className="box profileBox">
+                        <h2>Get <span className="colorText">Doctor</span> Recommended</h2>
+                        <p>Get <span className="colorText">Doctor</span> Recommended by us by answering a few questions</p>
+                        <div className="boxImg">
+                            <img src={boxImg} alt="" />
+                        </div>
+                        <Link className="btn">
+                            <div className="icn"> <MdArrowOutward/> </div>
+                            <a>Get Now</a>
+                        </Link>
                     </div>
-                    <div className="box">
-                        hdkjgvskjgcvd
+
+                    <div className="box profileBox">
+                        <h2><span className="colorText">Contact</span> Us!</h2>
+                        <p>Do you have any type of <span className="colorText">Query</span> Regarding us, Easily Contact Us...</p>
+                        <div className="boxImg">
+                            <img src={boxImg} alt="" />
+                        </div>
+                        <Link className="btn">
+                            <div className="icn"> <MdPermPhoneMsg/> </div>
+                            <a>Contact</a>
+                        </Link>
                     </div>
                 </div>
             </div>
